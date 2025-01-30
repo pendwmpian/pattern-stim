61669655746de741463b9addf255638b895b94d9
pendwmpian <22346581+pendwmpian@users.noreply.github.com>
2025-01-30 23:44:26 +0900

    make func detectOuterCircle

--- pattern-alignment.py
+++ pattern-alignment.py
@@ -7,23 +7,30 @@ RADIUS = 240
 # Load the image in grayscale
 image = cv2.imread('./data/input.bmp', cv2.IMREAD_GRAYSCALE)
 
-blurred = cv2.GaussianBlur(image, (5, 5), 1.5)
+def detectOuterCircle(image, radius=RADIUS):
 
-edges = cv2.Canny(blurred, threshold1=20, threshold2=10)
+    blurred = cv2.GaussianBlur(image, (5, 5), 1.5)
 
-radius = RADIUS
-circle_template = np.zeros((radius * 2, radius * 2), dtype=np.uint8)
-cv2.circle(circle_template, (radius, radius), radius, 255, -1)
+    edges = cv2.Canny(blurred, threshold1=20, threshold2=10)
 
-result = cv2.matchTemplate(edges, circle_template, cv2.TM_CCOEFF_NORMED)
+    radius = RADIUS
+    circle_template = np.zeros((radius * 2, radius * 2), dtype=np.uint8)
+    cv2.circle(circle_template, (radius, radius), radius, 255, -1)
 
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-center_x, center_y = max_loc[0] + radius, max_loc[1] + radius
+    result = cv2.matchTemplate(edges, circle_template, cv2.TM_CCOEFF_NORMED)
+
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    center_x, center_y = max_loc[0] + radius, max_loc[1] + radius
+
+    return center_x, center_y, radius
+
+
+center_x, center_y, radius = detectOuterCircle(image)
 
 # Draw the detected circle
+
 output_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 cv2.circle(output_image, (center_x, center_y), radius, (0, 255, 0), 2)
 
 # Save and display the result
 cv2.imwrite('output_image.jpg', output_image)
-
